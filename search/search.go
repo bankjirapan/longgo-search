deb61fb86bda7a8cb636a0ef8dc5db6e4f3e2732
bankjirapan <jirapan_yankhan@hotmail.com>
2025-01-15 17:18:43 +0700

    feat: search json data

--- search/search.go
+++ search/search.go
@@ -3,7 +3,10 @@ package search
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"strings"
+
+	"longgo-search.com/utils"
 )
 
 type Document struct {
@@ -11,46 +14,42 @@ type Document struct {
 	URL      string   `json:"url"`
 }
 
-func Search() {
+type Searcher interface {
+	Search(phrase string) string
+}
 
-	data := `[
-        {
-            "contents": [
-                "h1: การหาระยะห่างพิกัด",
-                "p:  ตัวอย่างนี้จะแสดงวิธีการหาระยะห่างระหว่างพิกัด 2 จุด "
-            ],
-            "url": "http://localhost:3000/docs3/ios/etc/distance-and-displacement"
-        },
-        {
-            "contents": [
-                "h1: เริ่มต้นการใช้งาน Longdo Map API",
-                "p:  บทความนี้จะแสดงวิธีการนำแผนที่ Longdo Map ไปใช้บนเว็บไซต์อย่างง่าย โดยนำไปประยุกต์ใช้ในงานของท่านได้ในหลากหลายรูปแบบ ไม่ว่าจะเป็นการนำ API (Application Programming Interface) ไปใช้ เรียกแสดงแผนที่ในเว็บหรือแอปพลิเคชันของท่าน, ติดตั้งผลิตภัณฑ์ Map Server ส่วนตัว ที่เรียกว่า Longdo Box ภายในเครือข่ายของท่าน (ดูหน้าผลิตภัณฑ์) เพื่อแยกการใช้งานแบบเป็นเอกเทศ ไม่ปะปนกับผู้ใช้รายอื่น, เพิ่มไอคอนของธุรกิจของท่านลงในแผนที่ รวมถึงป้องกันไม่ให้เกิดการแก้ไขจากบุคคลภายนอก ",
-                "p:  เข้าหน้าเว็บไซต์ map.longdo.com เพื่อสมัครสมาชิกของ Longdo Map ก่อน ",
-                "p:  เมื่อทำตามขั้นตอนทั้งหมดเสร็จสิ้น ท่านจะได้รับคีย์ยาว ๆ ในการใช้แพคเกจฟรี เช่น ",
-                "p:  หลังจากนักพัฒนาได้สร้างแผนที่พื้นฐานเรียบร้อยแล้ว ลำดับถัดไปจะเป็นการเพิ่มฟังก์ชันต่าง ๆ ที่ต้องการบนแผนที่ โดยหัวข้อด้านล่างนี้ "
-            ],
-            "url": "http://localhost:3000/docs3/react-native/getting-start"
-        }
-    ]`
+type Match struct {
+	URL     string   `json:"url"`
+	Heading string   `json:"heading"`
+	Text    []string `json:"text"`
+}
+
+func Search(phrase string) []Match {
+
+	data, err := ioutil.ReadFile("data/web-data.json")
 
 	var documents []Document
-	err := json.Unmarshal([]byte(data), &documents)
+	var matchingDocuments []Match
+	err = json.Unmarshal([]byte(data), &documents)
 	if err != nil {
 		fmt.Println("Error unmarshalling JSON:", err)
-		return
+		return nil
 	}
-
-	// Phrase to search for
-	phrase := "ห่างระหว่างพิกัด"
-
-	// Search for the phrase in the contents of each document
 	for _, doc := range documents {
 		for _, content := range doc.Contents {
-			if strings.Contains(content, phrase) {
-				fmt.Println("Found in URL:", doc.URL)
-				fmt.Println("Content:", content)
+			plainContent := strings.ToLower(utils.RemoveHTMLTags(content))
+			searchPhrase := strings.ToLower(phrase)
+			headingH1 := utils.FindH1Index(doc.Contents)
+
+			if strings.Contains(plainContent, searchPhrase) {
+				matchingDocuments = append(matchingDocuments, Match{
+					URL:     doc.URL,
+					Heading: utils.RemovePrefix(doc.Contents[headingH1]),
+					Text:    utils.RemovePrefixArray([]string{content}),
+				})
 			}
 		}
 	}
+	return matchingDocuments
 
 }
