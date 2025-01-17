307a1d48826c626f8a08d86db19d22683f9c3a80
bankjirapan <jirapan_yankhan@hotmail.com>
2025-01-17 11:47:01 +0700

    feat: flag mode crawler or server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import api "longgo-search.com/api"
+import (
+	"flag"
+	"log"
+
+	"longgo-search.com/api"
+	"longgo-search.com/crawler"
+)
 
 func main() {
-	api.StartServer()
+
+	mode := flag.String("mode", "server", "Mode to run the application: 'server' or 'crawler'")
+	url := flag.String("url", "", "URL to crawl (required if mode is 'crawler')")
+	flag.Parse()
+
+	switch *mode {
+	case "server":
+		log.Println("Starting HTTP Server...")
+		api.StartServer()
+	case "crawler":
+		if *url == "" {
+			log.Fatal("URL is required in crawler mode")
+		}
+		log.Println("Starting Crawler with URL:", *url)
+		crawler.Crawler(*url)
+	default:
+		log.Fatalf("Invalid mode: %s. Use 'server' or 'crawler'", *mode)
+	}
 }
